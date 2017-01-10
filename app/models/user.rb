f35f548c71212a69771896d6f290d0860752a06f
Hari Gopal <mail@harigopal.in>
2017-01-11 00:07:56 +0530

    Add devise_token_auth and a basic User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable
+
+  include DeviseTokenAuth::Concerns::User
+end
