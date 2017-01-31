70681198db997bab412f0b34d5c93ea032391440
Hari Gopal <mail@harigopal.in>
2017-01-31 14:27:53 +0530

    Minor change to how relationship is specified

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,7 +2,7 @@ class Volunteer < ApplicationRecord
   belongs_to :user
   belongs_to :coach
   has_one :fellow, through: :coach
-  has_one :national_finance_head, through: :coach
+  has_one :national_finance_head, through: :fellow
   has_many :donations
 
   validates_presence_of :user, :coach
