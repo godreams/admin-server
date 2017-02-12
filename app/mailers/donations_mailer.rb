60e677a9bec7453893f66bd1d5814e4e35fdf9d0
Hari Gopal <mail@harigopal.in>
2017-02-12 20:23:09 +0530

    test_message should accept a test_email. Doh!

--- app/mailers/donations_mailer.rb
+++ app/mailers/donations_mailer.rb
@@ -1,5 +1,5 @@
 class DonationsMailer < ApplicationMailer
-  def test_message
-    mail(subject: 'Test message', to: 'sheldon@bigbangtheory.com')
+  def test_message(test_email)
+    mail(subject: 'Test message', to: test_email)
   end
 end
