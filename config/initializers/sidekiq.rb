1d17a4854b9f02c90d329ded8fe0e5e2919a4e56
Hari Gopal <mail@harigopal.in>
2017-02-14 02:05:32 +0530

    Try configuring sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,14 @@
+# http://manuelvanrijn.nl/sidekiq-heroku-redis-calc/
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  # The config.redis is calculated by the
+  # concurrency value so you do not need to
+  # specify this. For this demo I do
+  # show it to understand the numbers
+  config.redis = { :size => 15 }
+end
