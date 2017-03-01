18831b6efb9e81608309783533cd62742b58b307
Hari Gopal <mail@harigopal.in>
2017-03-01 23:51:19 +0530

    Add missing name variable to donation_receipt SMS

--- app/jobs/donations/receipt_job.rb
+++ app/jobs/donations/receipt_job.rb
@@ -5,7 +5,7 @@ module Donations
       DonationsMailer.receipt(donation.id).deliver_now
 
       # Send an SMS as notification.
-      sms_service = SmsService.new(donation.phone, :donation_receipt, email: donation.email)
+      sms_service = SmsService.new(donation.phone, :donation_receipt, name: donation.name, email: donation.email)
       sms_service.execute
     end
   end
