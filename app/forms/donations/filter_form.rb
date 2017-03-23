465b1b893240a21c5edafabf2b0665e13364dc96
Hari Gopal <mail@harigopal.in>
2017-03-23 19:31:03 +0530

    Supply prepopulator to volunteer property

--- app/forms/donations/filter_form.rb
+++ app/forms/donations/filter_form.rb
@@ -1,10 +1,5 @@
 module Donations
   class FilterForm < Reform::Form
-    property :volunteer, virtual: true
-
-    # TODO: Shouldn't the default 'prepopulate!' work here? Not sure why it didn't when i tried.
-    def prepopulate!(options)
-      self.volunteer = options.dig(:volunteer)
-    end
+    property :volunteer, virtual: true, prepopulator: ->(options) { self.volunteer = options&.dig(:volunteer) }
   end
 end
