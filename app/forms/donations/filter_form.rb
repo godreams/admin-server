36624fbc21590c43f1f2805ee36eb6bbb0ce68de
Abdul Jaleel <jaleel@sv.co>
2017-03-29 23:09:41 +0530

    Add city to the filter form

--- app/forms/donations/filter_form.rb
+++ app/forms/donations/filter_form.rb
@@ -1,5 +1,6 @@
 module Donations
   class FilterForm < Reform::Form
     property :volunteer, virtual: true, prepopulator: ->(options) { self.volunteer = options&.dig(:volunteer) }
+    property :city, virtual: true, prepopulator: ->(options) { self.city = options&.dig(:city) }
   end
 end
--- app/services/donations/filter_service.rb
+++ app/services/donations/filter_service.rb
@@ -9,6 +9,10 @@ module Donations
         @donations = @donations.where(volunteer: Volunteer.find(filter_params[:volunteer]))
       end
 
+      if filter_params&.dig(:city).present?
+        @donations = @donations.merge(City.find(filter_params[:city]).donations)
+      end
+
       @donations
     end
   end
